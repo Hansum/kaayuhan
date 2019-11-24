92349a0207c1d2535730101d4128622972f03824
hansum <christianvargan@gmail.com>
2019-11-25 00:31:37 +0800

    woking with hooks

--- src/app.js
+++ src/app.js
@@ -1,28 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
 import Pet from "./Pet";
+import SearchParams from "./searchParams";
 
 const App = () => {
-  // return React.createElement("div", { className: "uhm hello?" }, [
-  //   React.createElement("p", { id: "firstId" }, "Hello World"),
-  //   React.createElement(Pet, { name: "butchi", animal: "dog", breed: "askal" }),
-  //   React.createElement(Pet, {
-  //     name: "bok",
-  //     animal: "pig",
-  //     breed: "brown pig"
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "bulter",
-  //     animal: "dog",
-  //     breed: "golden retriever"
-  //   })
-  // ]);
   return (
     <div>
       <h1 id="hello">My First react app</h1>
-      <Pet name="butchi" animal="dog" breed="askal"></Pet>
-      <Pet name="butter" animal="cat" breed="meow"></Pet>
-      <Pet name="crocy" animal="crocodile" breed="salt water crocodile"></Pet>
+      <SearchParams />
     </div>
   );
 };
---
+++ src/searchParams.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA"); //Location - current state of location while setLocation will be the updated location in the input. THIS IS A HOOK. Every time you see 'use', it is a hook
+  const [animal, setAnimal] = useState("dog");
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={event => setLocation(event.target.value)} //onChange will trigger if there are changes inside the input and it will call setLocation and passed the value based inside the input
+          ></input>
+        </label>
+        <label htmlFor="animal">
+          Animals
+          <select
+            id="animal"
+            value={animal}
+            onChange={event => setAnimal(event.target.value)}
+            onBlur={event => setAnimal(event.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option value={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
